9e0abf735e42699f1f610f2fe693e357a4be1505
Philipp Schmid <philipp@Philipps-MBP.fritz.box>
2018-12-22 21:30:12 +0100

    removed spinner for singlepage look

--- src/components/Header/Navigation.js
+++ src/components/Header/Navigation.js
@@ -4,46 +4,46 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {Link} from 'gatsby'
 import {OpacityTransition} from '../Transition'
-import {Flex, Box} from 'rebass'
+import {Flex} from 'rebass'
 import '../../assets/style/navigation.styl'
 
-const NAV_ITEMS = [
-	{
-		to: '/',
-		label: 'Index',
-	},
-	{
-		to: '/page-3',
-		label: 'Page 2',
-	},
-	{
-		to: '/page-4',
-		label: 'Test',
-	},
-	{
-		to: '/mdx-example',
-		label: 'Markdown Example',
-	},
+const nav_items = [
+  {
+    to: '/',
+    label: 'Index',
+  },
+  {
+    to: '/page-3',
+    label: 'Page 2',
+  },
+  {
+    to: '/page-4',
+    label: 'Test',
+  },
+  {
+    to: '/mdx-example',
+    label: 'Markdown Example',
+  },
 ]
 
-const NavList = NAV_ITEMS.map(e => {
-	return (
-		<li key={e.to}>
-			<Link className="li-level1" to={e.to}>
-				{e.label}
-			</Link>
-		</li>
-	)
+const NavList = nav_items.map(e => {
+  return (
+    <li key={e.to}>
+      <Link className="li-level1" to={e.to}>
+        {e.label}
+      </Link>
+    </li>
+  )
 })
 
 export const Navigation = props => (
-	<OpacityTransition className={`overlay-navigation ${props.isMenuVisible ? 'overlay-slide-down' : ''}`}>
-		<Flex justifyContent="center" alignItems="center" style={{height: '100%', width: '100%'}}>
-			<ul className="level1">{NavList}</ul>
-		</Flex>
-	</OpacityTransition>
+  <OpacityTransition className={`overlay-navigation ${props.isMenuVisible ? 'overlay-slide-down' : ''}`}>
+    <Flex justifyContent="center" alignItems="center" style={{height: '100%', width: '100%'}}>
+      <ul className="level1">{NavList}</ul>
+    </Flex>
+  </OpacityTransition>
 )
 
 Navigation.propTypes = {
-	onToggleMenu: PropTypes.func,
+  onToggleMenu: PropTypes.func,
 }
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -16,66 +16,66 @@ body {
 `
 
 class Layout extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			isMenuVisible: false,
-			loading: 'is-loading',
-		}
-		console.log(this.state)
-		this.handleToggleMenu = this.handleToggleMenu.bind(this)
-	}
+  constructor(props) {
+    super(props)
+    this.state = {
+      isMenuVisible: false,
+      loading: 'is-loading',
+    }
+    console.log(this.state)
+    this.handleToggleMenu = this.handleToggleMenu.bind(this)
+  }
 
-	componentDidMount() {
-		this.timeoutId = setTimeout(() => {
-			this.setState({loading: ''})
-		}, 100)
-	}
+  componentDidMount() {
+    this.timeoutId = setTimeout(() => {
+      this.setState({loading: ''})
+    }, 100)
+  }
 
-	componentWillUnmount() {
-		if (this.timeoutId) {
-			clearTimeout(this.timeoutId)
-		}
-	}
+  componentWillUnmount() {
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId)
+    }
+  }
 
-	handleToggleMenu() {
-		this.setState({
-			isMenuVisible: !this.state.isMenuVisible,
-		})
-	}
+  handleToggleMenu() {
+    this.setState({
+      isMenuVisible: !this.state.isMenuVisible,
+    })
+  }
 
-	render() {
-		const {children} = this.props
-		if (this.state.loading === 'is-loading') {
-			return <Spinner />
-		} else {
-			return (
-				<div className={`body ${this.state.loading} ${this.state.isMenuVisible ? 'is-menu-visible' : ''}`}>
-					<GlobalStyle />
-					<ThemeProvider theme={theme}>
-						<div>
-							<Header onToggleMenu={this.handleToggleMenu} isMenuVisible={this.state.isMenuVisible} />
-							<div
-								style={{
-									margin: '0 auto',
-									maxWidth: '40rem',
-									padding: '0px 1.0875rem 1.45rem',
-									paddingTop: 0,
-								}}>
-								{children}
-							</div>
-							{this.state.isMenuVisible && (
-								<Navigation onToggleMenu={this.handleToggleMenu} isMenuVisible={this.state.isMenuVisible} />
-							)}
-						</div>
-					</ThemeProvider>
-				</div>
-			)
-		}
-	}
+  render() {
+    const {children} = this.props
+    // if (this.state.loading === 'is-loading') {
+    //   return <Spinner />
+    // } else {
+    return (
+      <div className={`body ${this.state.loading} ${this.state.isMenuVisible ? 'is-menu-visible' : ''}`}>
+        <GlobalStyle />
+        <ThemeProvider theme={theme}>
+          <div>
+            <Header onToggleMenu={this.handleToggleMenu} isMenuVisible={this.state.isMenuVisible} />
+            <div
+              style={{
+                margin: '0 auto',
+                maxWidth: '40rem',
+                padding: '0px 1.0875rem 1.45rem',
+                paddingTop: 0,
+              }}>
+              {children}
+            </div>
+            {this.state.isMenuVisible && (
+              <Navigation onToggleMenu={this.handleToggleMenu} isMenuVisible={this.state.isMenuVisible} />
+            )}
+          </div>
+        </ThemeProvider>
+      </div>
+    )
+  }
+  //   }
 }
 Layout.propTypes = {
-	children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 }
 
 export {Layout}
