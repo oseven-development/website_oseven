d868ccf27a34badb1987f114b181a99a4bcaee63
Philipp Schmid <philipp@Philipps-MBP.fritz.box>
2018-12-29 15:09:04 +0100

    removed old_pages

--- src/components/Background/Parallax.js
+++ src/components/Background/Parallax.js
@@ -1,9 +1,9 @@
+/** @format */
 
 import React from 'react'
 
 import {StyledParallax} from './styled'
 
 export const Parallax = ({children, ...props}) => {
-  console.log(props)
   return <StyledParallax {...props}>{children}</StyledParallax>
 }
