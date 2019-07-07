1271473a2adb6f9ab32b15a578163bda91e7d650
Philipp Schmid <philipp@Philipps-MBP.fritz.box>
2019-07-07 18:23:57 +0200

    edit fonts

--- src/components/layout/globalStyle.ts
+++ src/components/layout/globalStyle.ts
@@ -21,14 +21,6 @@ export default createGlobalStyle`
 @font-face {
   font-family: 'Khula';
   font-style: normal;
-  font-weight: 300;
-  font-display: swap;
-  src: local('Khula Light'), local('Khula-Light'), url(https://fonts.gstatic.com/s/khula/v4/OpNPnoEOns3V7G-ljBvTpi9fXBXC.woff2) format('woff2');
-  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-}
-@font-face {
-  font-family: 'Khula';
-  font-style: normal;
   font-weight: 400;
   font-display: swap;
   src: local('Khula Regular'), local('Khula-Regular'), url(https://fonts.gstatic.com/s/khula/v4/OpNCnoEOns3V7GcOrg7-hCJ1.woff2) format('woff2');
