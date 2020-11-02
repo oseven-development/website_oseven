2236ea30e4e4e1aac3dcfb1e2e93d4c37155cd1c
Maximilian Hänsel <mh@maxhaensel.de>
2020-11-02 20:54:34 +0100

    ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -9,7 +9,8 @@
 // You can delete this file if you're not using it
 
 import React from 'react'
-import {Layout} from './src/components'
+import {Layout} from './src/layout'
+import './src/styles/global.scss'
 
 // export const onClientEntry = () => {
 //   // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
@@ -22,5 +23,5 @@ import {Layout} from './src/components'
 export const wrapPageElement = ({element, props}) => (
   // props provide same data to Layout as Page element will get
   // including location, data, etc - you don't need to pass it
-  <Layout {...props}> {element}</Layout>
+  <Layout {...props}>{element}</Layout>
 )
