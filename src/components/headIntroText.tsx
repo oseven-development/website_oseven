76d70ea9bf23f53b589f97d44a47d95dc6ed70e2
Stefan Blos <stefan.blos@gmail.com>
2020-11-23 00:13:17 +0100

    reworked contact section

--- src/components/headIntroText.tsx
+++ src/components/headIntroText.tsx
@@ -1,20 +1,29 @@
 /** @format */
 import React from 'react'
-import Phone from '../assets/svg/phone.svg'
+import PhoneIcon from '../assets/svg/phone.svg'
+import MailIcon from '../assets/svg/mail.svg'
 interface Props {
   children: string
   english?: boolean
 }
 export default ({children, english = false}: Props) => (
-  <section className="pt-16 md:pt-32 pb-16 md:pb-32">
+  <section className="py-16 md:py-32">
     <h1 className="text-4xl md:text-6xl md:w-9/12">{children}</h1>
-    <a href="tel:+491634412159" className="flex transform hover:-translate-y-1 duration-300 ease-in-out">
-      <div className="w-16 h-16 bg-secondary rounded-full p-4 mr-2 flex-shrink-0">
-        <Phone className=" fill-current text-white " />
-      </div>
-      <h2 className="flex items-center font-hairline m-0">
-        {!english ? 'Lass uns sprechen!' : 'Let us speak'} 0163-44-12-159
-      </h2>
-    </a>
+    <h2 className="w-1/2 md:w-1/3 inline-block border-t-2 border-white pt-2">{english ? "Contact us" : "Kontaktiere uns"}</h2>
+    <div className="space-y-4">
+      <p>
+        <a href="tel:+491634412159" className="flex items-center space-x-4 transform hover:-translate-y-1 duration-300 ease-in-out">
+          <PhoneIcon className="w-12 h-12 fill-current text-secondary p-3" />
+          <span className="text-lg">+49 163-44-12-159</span>
+        </a>
+      </p>
+
+      <p>
+        <a href="mailto:max@oseven.de" className="flex items-center space-x-4 transform hover:-translate-y-1 duration-300 ease-in-out">
+          <MailIcon className="w-12 h-12 fill-current text-secondary p-1" />
+          <span className="text-lg pb-2">max@oseven.de</span>
+        </a>
+      </p>
+    </div>
   </section>
 )
