1ad88ea99248d24529b8e82c8cb80dd2f95a5cf8
Maximilian Hänsel <mh@maxhaensel.de>
2020-11-26 20:57:19 +0100

    wip index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,11 @@ import React from 'react'
 import {graphql, Link, useStaticQuery} from 'gatsby'
 import Img from 'gatsby-image'
 import {ImageQuery} from '..'
-import {HeadTextIntro, Seo} from '../components'
-// import {SVGteam, SVGprofessor} from '../assets'
+import {HeadTextIntro, Seo, A, OfferCard} from '../components'
+import AWSLogo from '../assets/svg/Amazon_Web_Services_Logo.svg'
+import GCPLogo from '../assets/svg/google_cloud.svg'
+import PytorchLogo from '../assets/svg/pytorch.svg'
+
 // import SiteContent from '../assets/content/uberuns'
 // Components
 // import {Seo, Particles, SkillCardBox, PortraitBox, ContentDivider} from '../components'
@@ -46,19 +49,54 @@ export default () => {
         image={image.file.childImageSharp.fluid.src}
       />
       {/* <HeadTextIntro>Wir sind Spezialisten für Künstliche Intelligenz (KI) und Cloud-Entwicklung</HeadTextIntro> */}
-      <HeadTextIntro>IT und Business Model Mentoring für Startups</HeadTextIntro>
+      <HeadTextIntro>Software und Business Model Mentoring</HeadTextIntro>
 
       <section>
         <Img ref={ref} fluid={image.file.childImageSharp.fluid} alt={'name'} className="max-h-80" />
       </section>
 
+      <div className="bg-white text-black">
+        <section className="py-12">
+          <div className="grid  grid-cols-1 lg:grid-cols-3 gap-6">
+            <OfferCard
+              {...{
+                title: 'Cloud-Workloads',
+                logo: AWSLogo,
+                content: [
+                  'Migration von on-premise workloads in die Cloud.',
+                  'Mehrfach von AWS zertifiziert.',
+                  'Beratung zu Architekturen und das Aufbauen von verteilten Systemen.',
+                  'Aufbau von Release Management und CI-CD pipelines.',
+                  'Beratung und Migration zu Kubernetes',
+                ],
+                technologie: [
+                  {href: 'https://aws.amazon.com/de/', text: 'aws'},
+                  {href: 'https://cloud.google.com/', text: 'gcp'},
+                  {href: 'https://github.com', text: 'github'},
+                  {href: 'https://kubernetes.io/de/', text: 'kubernetes'},
+                ],
+              }}
+            />
+
+            <div className="shadow-2xl px-4 py-8">
+              <GCPLogo className="h-16 mx-auto" />
+              <h2>So Businesszeugs</h2>
+              <p className="text-base leading-6">Was der Lars so macht?</p>
+            </div>
+
+            <div className="shadow-2xl px-4 py-8">
+              <PytorchLogo className="h-16 mx-auto" />
+              <h2>pytorch</h2>
+              <p className="text-base leading-6">
+                Mehrfach zertifiziert in AWS, Microsoft Azure und Google Cloud Platform.
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
+
       <section className="py-12">
         <p className="md:w-6/12 mb-8">
-          {/* Um in schnell verändernden Märkten wettbewerbsfähig zu bleiben, müssen Sie in Ihre digitalen Prozesse, die
-          Infrastruktur und deren Architektur investieren. Wir unterstützen Sie dabei bestmöglich und finden gemeinsam
-          mit Ihnen Lösungen für die Fragen von heute und Probleme von Morgen. Dazu entwickeln wir native
-          Cloud-Lösungen, die genauso flexibel und agil wachsen, wie ihr Unternehmen, sich aber zugleich nach Ihren
-          individuellen Bedürfnissen richten. */}
           Mit unserer Expertise und Erfahrung helfen wir dir, deine Geschäftsidee in ein nachhaltiges Geschäftsmodell zu
           überführen und schnelle und effiziente Softwarelösungen umzusetzen!
         </p>
@@ -70,8 +108,8 @@ export default () => {
       <div className="bg-white">
         <section className=" flex flex-col">
           <h2 className="text-4xl text-center text-primary md:w-6/12 mx-auto mb-8">
-            Wir entwickeln maßgeschneiderte Softwarelösungen und helfen dir bei der Migration deiner
-            Anwendungen in die Cloud.
+            Wir entwickeln maßgeschneiderte Softwarelösungen und helfen dir bei der Migration deiner Anwendungen in die
+            Cloud.
           </h2>
           <Link className="rounded-full bg-primary text-white p-6 inline-block mx-auto" to="UnsereArbeit">
             Unsere Arbeit
