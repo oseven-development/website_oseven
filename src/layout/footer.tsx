a225b303fdc2df48074e20955ffa1329f9826af8
Maximilian Hänsel <mh@maxhaensel.de>
2021-01-19 19:33:24 +0100

    remove misspelling

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -11,9 +11,7 @@ interface Props {
 export default ({langKey}: Props) => (
   <footer className="flex flex-col py-12">
     <p className="mx-auto md:w-1/2 text-center p-12 md:text-2xl">
-      {langKey === 'de'
-        ? 'Wir freuen uns, mit dir zusammenzuarbeiten.'
-        : 'We&apos;d love to work with you.'}
+      {langKey === 'de' ? 'Wir freuen uns, mit dir zusammenzuarbeiten.' : 'We love to work with you.'}
     </p>
     <LinkButton to={`/${langKey === 'de' ? '' : langKey + '/'}kontakt`} primary={false}>
       {langKey === 'de' ? 'Kontakt' : 'Contact'}
